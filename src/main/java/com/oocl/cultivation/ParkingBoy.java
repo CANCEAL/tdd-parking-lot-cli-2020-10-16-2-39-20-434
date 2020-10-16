55e77e23baec8ed2075041b9b5ed3a7808596733
CANCEAL <alfred.cancejo@oocl.com>
2020-10-16 14:04:14 +0800

    [Parking Lot] [Alfred] Feature: Code to return parking ticket when car is given to parking boy

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,10 +1,15 @@
 package com.oocl.cultivation;
 
 public class ParkingBoy {
+    private ParkingLot parkingLot;
+
     public ParkingBoy(ParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
     }
 
     public ParkingTicket park(Car car) {
-        return null;
+        return parkingLot.park(car);
     }
+
+
 }
