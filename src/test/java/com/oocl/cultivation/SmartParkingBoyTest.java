3d7c69fa0eacde52f89e9bfe826b913b543290d9
CANCEAL <alfred.cancejo@oocl.com>
2020-10-18 11:30:06 +0800

    [Parking Lot] [Alfred] Test: case to park car on parking lot which has highest position rate when parking given a car

---
+++ src/test/java/com/oocl/cultivation/SmartParkingBoyTest.java
@@ -0,0 +1,36 @@
+package com.oocl.cultivation;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class SmartParkingBoyTest extends ParkingBoyTest {
+
+    @Test
+    void should_park_to_parking_lot_which_has_more_empty_space_when_parking_given_parking_car() {
+        //given
+        ParkingLot parkingLot1 = new ParkingLot();
+        ParkingLot parkingLot2 = new ParkingLot();
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLot1);
+        parkMultipleCars(smartParkingBoy, 4);
+
+        smartParkingBoy = new SmartParkingBoy(parkingLot2);
+        parkMultipleCars(smartParkingBoy, 8);
+
+        smartParkingBoy = new SmartParkingBoy(parkingLots);
+
+        //when
+        Car car1 = new Car();
+        ParkingTicket parkingTicket = smartParkingBoy.park(car1);
+
+        //then
+        assertNotNull(parkingLot1.fetch(parkingTicket));
+    }
+}
