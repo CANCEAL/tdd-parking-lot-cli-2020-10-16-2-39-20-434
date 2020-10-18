66c81fd4f6d536b23d8667de93b06f1e547037ae
CANCEAL <alfred.cancejo@oocl.com>
2020-10-18 17:19:41 +0800

    [Parking Lot] [Alfred] Feature: code to park service manager on parking lots he owned given a parking car

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -5,6 +5,7 @@ import exceptions.UnrecognizedParkingTicketException;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collector;
 
 public class ParkingBoy {
     private List<ParkingLot> parkingLots;
--- src/main/java/com/oocl/cultivation/ServiceManager.java
+++ src/main/java/com/oocl/cultivation/ServiceManager.java
@@ -1,6 +1,7 @@
 package com.oocl.cultivation;
 
 import exceptions.NotEnoughPositionException;
+import exceptions.UnrecognizedParkingTicketException;
 
 import java.util.List;
 
@@ -8,18 +9,15 @@ public class ServiceManager extends ParkingBoy{
     List<ParkingLot> parkingLots;
     List<ParkingBoy> parkingBoys;
 
+
+    public ServiceManager(List<ParkingLot> parkingLots) {
+        super(parkingLots);
+        this.parkingLots = parkingLots;
+    }
+
     public ServiceManager(List<ParkingLot> parkingLots, List<ParkingBoy> parkingBoys) {
         super(parkingLots, parkingBoys);
         this.parkingLots = parkingLots;
         this.parkingBoys = parkingBoys;
-    }
-
-    public ParkingTicket park(Car car) {
-        ParkingLot parkingLot = parkingLots.stream().filter(ParkingLot -> ParkingLot.isParkingLotFull()).findFirst().orElse(null);;
-
-        if (parkingLot == null) {
-            throw new NotEnoughPositionException("Not Enough Position!");
-        }
-        return parkingLot.park(car);
     }
 }
