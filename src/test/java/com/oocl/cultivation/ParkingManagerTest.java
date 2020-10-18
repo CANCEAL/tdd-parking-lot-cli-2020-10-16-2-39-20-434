ee72c47a96b3383c753f41aac905b8b9cf3105e7
CANCEAL <alfred.cancejo@oocl.com>
2020-10-18 21:24:59 +0800

    [Parking Lot] [Alfred] Refactor: updated cases of ParkingManagerTest

--- src/test/java/com/oocl/cultivation/ParkingManagerTest.java
+++ src/test/java/com/oocl/cultivation/ParkingManagerTest.java
@@ -1,6 +1,5 @@
 package com.oocl.cultivation;
 
-import exceptions.NotEnoughPositionException;
 import exceptions.UnrecognizedParkingTicketException;
 import org.junit.jupiter.api.Test;
 
@@ -9,30 +8,22 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ServiceManagerTest extends ParkingBoyTest{
+public class ParkingManagerTest extends ParkingBoyTest{
     @Test
-    public void should_add_to_management_list_when_managing_given_parking_boys() {
+    public void should_parking_manager_add_to_management_list_when_managing_given_parking_boys() {
         //given
         ParkingLot parkingLot1 = new ParkingLot();
         ParkingLot parkingLot2 = new ParkingLot();
-        ParkingLot parkingLot3 = new ParkingLot();
-        List<ParkingLot> parkingLots = new ArrayList<>();
-        parkingLots.add(parkingLot1);
-        parkingLots.add(parkingLot2);
-        parkingLots.add(parkingLot3);
 
         ParkingBoy parkingBoy1 = new ParkingBoy(parkingLot1);
         ParkingBoy parkingBoy2 = new ParkingBoy(parkingLot2);
-        ParkingBoy parkingBoy3 = new ParkingBoy(parkingLot3);
-        List<ParkingBoy> parkingBoys = new ArrayList<>();
-        parkingBoys.add(parkingBoy1);
-        parkingBoys.add(parkingBoy2);
-        parkingBoys.add(parkingBoy3);
+
+        ParkingManager parkingManager = new ParkingManager(parkingBoy1);
+        parkingManager = new ParkingManager(parkingBoy2);
 
         //when
         Car car1 = new Car();
-        ServiceManager serviceManager = new ServiceManager(parkingLots, parkingBoys);
-        ParkingTicket parkingTicket1 = serviceManager.park(car1);
+        ParkingTicket parkingTicket1 = parkingBoy1.park(car1);
 
         //then
         assertNotNull(parkingBoy1.fetch(parkingTicket1));
@@ -43,23 +34,26 @@ public class ServiceManagerTest extends ParkingBoyTest{
         //given
         ParkingLot parkingLot1 = new ParkingLot();
         ParkingLot parkingLot2 = new ParkingLot();
-        ParkingLot parkingLot3 = new ParkingLot();
         List<ParkingLot> parkingLots = new ArrayList<>();
         parkingLots.add(parkingLot1);
         parkingLots.add(parkingLot2);
-        parkingLots.add(parkingLot3);
 
-        ServiceManager serviceManager = new ServiceManager(parkingLots);
+        ParkingBoy parkingBoy1 = new ParkingBoy(parkingLot1);
+        ParkingBoy parkingBoy2 = new ParkingBoy(parkingLot2);
+        List<ParkingBoy> parkingBoys = new ArrayList<>();
+        parkingBoys.add(parkingBoy1);
+        parkingBoys.add(parkingBoy2);
+
+        ParkingManager parkingManager = new ParkingManager(parkingLots, parkingBoys);
 
         //when
-        parkMultipleCars(serviceManager, 10);
+        parkMultipleCars(parkingManager, 10);
         Car car11 = new Car();
-        ParkingTicket parkingTicket = serviceManager.park(car11);
+        ParkingTicket parkingTicket = parkingManager.park(car11);
 
         //then
         assertNull(parkingLot1.fetch(parkingTicket));
         assertNotNull(parkingLot2.fetch(parkingTicket));
-        assertNull(parkingLot3.fetch(parkingTicket));
     }
     
     @Test
@@ -67,23 +61,19 @@ public class ServiceManagerTest extends ParkingBoyTest{
         //given
         ParkingLot parkingLot1 = new ParkingLot();
         ParkingLot parkingLot2 = new ParkingLot();
-        ParkingLot parkingLot3 = new ParkingLot();
         List<ParkingLot> parkingLots = new ArrayList<>();
         parkingLots.add(parkingLot1);
         parkingLots.add(parkingLot2);
-        parkingLots.add(parkingLot3);
 
         ParkingBoy parkingBoy1 = new ParkingBoy(parkingLot1);
         ParkingBoy parkingBoy2 = new ParkingBoy(parkingLot2);
-        ParkingBoy parkingBoy3 = new ParkingBoy(parkingLot3);
         List<ParkingBoy> parkingBoys = new ArrayList<>();
         parkingBoys.add(parkingBoy1);
         parkingBoys.add(parkingBoy2);
-        parkingBoys.add(parkingBoy3);
 
         Car car1 = new Car();
-        ServiceManager serviceManager = new ServiceManager(parkingLots, parkingBoys);
-        ParkingTicket parkingTicket1 = serviceManager.park(car1);
+        ParkingManager parkingManager = new ParkingManager(parkingLots, parkingBoys);
+        ParkingTicket parkingTicket1 = parkingManager.park(car1);
 
         //then
         assertThrows(UnrecognizedParkingTicketException.class, () ->
