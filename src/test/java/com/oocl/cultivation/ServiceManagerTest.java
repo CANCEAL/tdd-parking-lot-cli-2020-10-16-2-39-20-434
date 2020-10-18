96d7f273cc1bc30b7851ad1270a594620e4e91ef
CANCEAL <alfred.cancejo@oocl.com>
2020-10-18 17:33:41 +0800

    [Parking Lot] [Alfred] Test: case throw error message when operation failed given parking boy fetch car from different parking lot

--- src/test/java/com/oocl/cultivation/ServiceManagerTest.java
+++ src/test/java/com/oocl/cultivation/ServiceManagerTest.java
@@ -1,5 +1,7 @@
 package com.oocl.cultivation;
 
+import exceptions.NotEnoughPositionException;
+import exceptions.UnrecognizedParkingTicketException;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -58,5 +60,36 @@ public class ServiceManagerTest extends ParkingBoyTest{
         assertNull(parkingLot1.fetch(parkingTicket));
         assertNotNull(parkingLot2.fetch(parkingTicket));
         assertNull(parkingLot3.fetch(parkingTicket));
+    }
+    
+    @Test
+    public void should_return_error_message_when_operation_failed_given_parking_boy_fetch_car() {
+        //given
+        ParkingLot parkingLot1 = new ParkingLot();
+        ParkingLot parkingLot2 = new ParkingLot();
+        ParkingLot parkingLot3 = new ParkingLot();
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+        parkingLots.add(parkingLot3);
+
+        ParkingBoy parkingBoy1 = new ParkingBoy(parkingLot1);
+        ParkingBoy parkingBoy2 = new ParkingBoy(parkingLot2);
+        ParkingBoy parkingBoy3 = new ParkingBoy(parkingLot3);
+        List<ParkingBoy> parkingBoys = new ArrayList<>();
+        parkingBoys.add(parkingBoy1);
+        parkingBoys.add(parkingBoy2);
+        parkingBoys.add(parkingBoy3);
+
+        Car car1 = new Car();
+        ServiceManager serviceManager = new ServiceManager(parkingLots, parkingBoys);
+        ParkingTicket parkingTicket1 = serviceManager.park(car1);
+
+        //then
+        assertThrows(UnrecognizedParkingTicketException.class, () ->
+
+                //when
+                parkingBoy2.fetch(parkingTicket1)
+        );
     }
 }
