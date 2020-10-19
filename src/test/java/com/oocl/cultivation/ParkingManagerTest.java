6178ad53daffcef3285b5e6c2b0df157dd2c9970
CANCEAL <alfred.cancejo@oocl.com>
2020-10-19 18:45:44 +0800

    [Parking Lot] [Alfred] Test: created ParkingManagerTest methods

--- src/test/java/com/oocl/cultivation/ParkingManagerTest.java
+++ src/test/java/com/oocl/cultivation/ParkingManagerTest.java
@@ -13,20 +13,16 @@ public class ParkingManagerTest extends ParkingBoyTest{
     public void should_parking_manager_add_to_management_list_when_managing_given_parking_boys() {
         //given
         ParkingLot parkingLot1 = new ParkingLot();
-        ParkingLot parkingLot2 = new ParkingLot();
-
         ParkingBoy parkingBoy1 = new ParkingBoy(parkingLot1);
-        ParkingBoy parkingBoy2 = new ParkingBoy(parkingLot2);
-
-        ParkingManager parkingManager = new ParkingManager(parkingBoy1);
-        parkingManager = new ParkingManager(parkingBoy2);
+        ParkingManager parkingManager = new ParkingManager(parkingLot1);
+        parkingManager.addParkingBoy(parkingBoy1);
 
         //when
         Car car1 = new Car();
-        ParkingTicket parkingTicket1 = parkingBoy1.park(car1);
+        ParkingTicket parkingTicket1 = parkingManager.park(parkingBoy1, car1);
 
         //then
-        assertNotNull(parkingBoy1.fetch(parkingTicket1));
+        assertNotNull(parkingManager.fetch(parkingBoy1, parkingTicket1));
     }
     
     @Test
@@ -37,49 +33,35 @@ public class ParkingManagerTest extends ParkingBoyTest{
         List<ParkingLot> parkingLots = new ArrayList<>();
         parkingLots.add(parkingLot1);
         parkingLots.add(parkingLot2);
-
-        ParkingBoy parkingBoy1 = new ParkingBoy(parkingLot1);
-        ParkingBoy parkingBoy2 = new ParkingBoy(parkingLot2);
-        List<ParkingBoy> parkingBoys = new ArrayList<>();
-        parkingBoys.add(parkingBoy1);
-        parkingBoys.add(parkingBoy2);
-
-        ParkingManager parkingManager = new ParkingManager(parkingLots, parkingBoys);
+        ParkingManager parkingManager = new ParkingManager(parkingLots);
 
         //when
-        parkMultipleCars(parkingManager, 10);
-        Car car11 = new Car();
-        ParkingTicket parkingTicket = parkingManager.park(car11);
+        Car car1 = new Car();
+        ParkingTicket parkingTicket = parkingManager.park(car1);
 
         //then
-        assertNull(parkingLot1.fetch(parkingTicket));
-        assertNotNull(parkingLot2.fetch(parkingTicket));
+        assertNotNull(parkingManager.fetch(parkingTicket));
     }
     
     @Test
     public void should_return_error_message_when_operation_failed_given_parking_boy_fetch_car() {
         //given
         ParkingLot parkingLot1 = new ParkingLot();
-        ParkingLot parkingLot2 = new ParkingLot();
-        List<ParkingLot> parkingLots = new ArrayList<>();
-        parkingLots.add(parkingLot1);
-        parkingLots.add(parkingLot2);
-
         ParkingBoy parkingBoy1 = new ParkingBoy(parkingLot1);
-        ParkingBoy parkingBoy2 = new ParkingBoy(parkingLot2);
-        List<ParkingBoy> parkingBoys = new ArrayList<>();
-        parkingBoys.add(parkingBoy1);
-        parkingBoys.add(parkingBoy2);
+        ParkingManager parkingManager = new ParkingManager(parkingLot1);
+        parkingManager.addParkingBoy(parkingBoy1);
 
+        //when
         Car car1 = new Car();
-        ParkingManager parkingManager = new ParkingManager(parkingLots, parkingBoys);
-        ParkingTicket parkingTicket1 = parkingManager.park(car1);
+        ParkingTicket parkingTicket1 = parkingManager.park(parkingBoy1, car1);
+        ParkingTicket parkingTicket2 = new ParkingTicket();
 
         //then
+        assertNotNull(parkingManager.fetch(parkingBoy1, parkingTicket1));
         assertThrows(UnrecognizedParkingTicketException.class, () ->
 
                 //when
-                parkingBoy2.fetch(parkingTicket1)
+                parkingManager.fetch(parkingBoy1, parkingTicket2)
         );
     }
 }
