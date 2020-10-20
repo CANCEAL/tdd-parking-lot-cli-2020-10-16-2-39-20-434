e0b41500b3625c878493ee67fa3661675d4dd1a9
CANCEAL <alfred.cancejo@oocl.com>
2020-10-20 20:42:34 +0800

    [Parking Lot] [Alfred] Refactor: updated superSmartParkingBoy test method

--- src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
@@ -2,9 +2,6 @@ package com.oocl.cultivation;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SuperSmartParkingBoyTest extends ParkingBoyTest{
@@ -14,15 +11,12 @@ public class SuperSmartParkingBoyTest extends ParkingBoyTest{
         //given
         ParkingLot parkingLot1 = new ParkingLot(15);
         ParkingLot parkingLot2 = new ParkingLot(20);
-        List<ParkingLot> parkingLots = new ArrayList<>();
-        parkingLots.add(parkingLot1);
-        parkingLots.add(parkingLot2);
 
         SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(parkingLot1);
         parkMultipleCars(superSmartParkingBoy, 3);
         superSmartParkingBoy = new SuperSmartParkingBoy(parkingLot2);
         parkMultipleCars(superSmartParkingBoy, 5);
-        superSmartParkingBoy = new SuperSmartParkingBoy(parkingLots);
+        superSmartParkingBoy = new SuperSmartParkingBoy(parkingLot1, parkingLot2);
 
         //when
         Automobile automobile = new Automobile();
